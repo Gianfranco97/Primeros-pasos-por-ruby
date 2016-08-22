3277e6a6c50618fbab2a0e2943e93c83227cb6fa
Gianfranco <gianfrancomanganiello@hotmail.com>
2016-08-21 21:28:08 -0400

    mas cambios y agregados para mis primeros pasos

---
+++ TablaBasica.rb
@@ -0,0 +1,5 @@
+anchoLinea = 50
+puts "\n\n\n" + "Tabla de contenidos\n".center(anchoLinea)
+puts "Capítulo 1:   Números".ljust(anchoLinea/2) + "página 1".rjust(anchoLinea/2)
+puts "Capítulo 2:   Letras".ljust(anchoLinea/2) + "página 72".rjust(anchoLinea/2)
+puts "Capítulo 1:   Variables".ljust(anchoLinea/2) + "página 118".rjust(anchoLinea/2)
--- nombre_completo.rb
+++ nombre_completo.rb
@@ -6,4 +6,4 @@ puts "Estupendo, ¿Ahora podrias decirme tu primer apellido?"
 apellido1 = gets.chomp
 puts "Muy bien. Una ultima pregunta, ¿Tu segundo apellido?"
 apellido2 = gets.chomp
-puts "Tu nombre completo es: " + nombre1 + " " + nombre2 + " " + apellido1 + " " + apellido2 
+puts "Tu nombre completo es: " + nombre1.to_s + " " + nombre2.to_s + " " + apellido1.to_s + " " + apellido2.to_s
